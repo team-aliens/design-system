4565772f2516dc7aa7eb11103f5f180a04ea5b25
Josanghyeon <BStkdgus.dev@gmail.com>
2022-12-22 13:34:54 +0900

    fix: checkBox 매개변수 onChange로 변경

--- src/components/checkBox/index.tsx
+++ src/components/checkBox/index.tsx
@@ -9,7 +9,7 @@ interface PropsType extends marginCssType {
   disabled?: boolean;
   label?: string;
   status: boolean;
-  checkSetState: Dispatch<SetStateAction<boolean>>;
+  onChange: (status: boolean) => void;
 }
 
 /** checkBox 여러개 있을 때 처리를 딱히 안해둠 (사용할 때 고민해봅시다) */
@@ -18,14 +18,14 @@ export const CheckBox = ({
   disabled = false,
   label,
   status,
-  checkSetState,
+  onChange,
   margin,
 }: PropsType) => {
   return (
     <_Wrapper
       className={className}
       disabled={disabled}
-      onClick={() => !disabled && checkSetState(!status)}
+      onClick={() => !disabled && onChange(!status)}
       margin={margin}
     >
       <Box status={status} disabled={disabled} />
