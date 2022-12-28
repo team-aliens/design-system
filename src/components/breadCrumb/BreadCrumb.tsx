ad3740b0994bdc0cd6a274a3361356a79aa21d9d
Josanghyeon <BStkdgus.dev@gmail.com>
2022-12-28 23:45:16 +0900

    fix: BreadCrumb

--- src/components/breadCrumb/BreadCrumb.tsx
+++ src/components/breadCrumb/BreadCrumb.tsx
@@ -16,7 +16,8 @@ export const BreadCrumb = ({
   top = 86,
   left = 330,
 }: PropsType) => {
-  const path: string[] = useLocation().pathname.split('/').slice(1);
+  const path: string[] = ['my-page', 'change-pwd'];
+  // useLocation().pathname.split('/').slice(1);
   const pathList = [];
   const LinkList = [`/${path[0]}`];
   for (let i = 0; i < path.length; i++) {
@@ -24,6 +25,8 @@ export const BreadCrumb = ({
     if (pathToKorean[path[i]]) {
       if (pathToKorean[path[i]].index)
         pathList.push(pathToKorean[path[i]].index);
+      else if (typeof pathToKorean[path[i]] === 'string')
+        pathList.push(pathToKorean[path[i]]);
       pathToKorean = pathToKorean[path[i]];
     } else if (pathToKorean['dynamic']) {
       pathList.push(pathToKorean['dynamic']);
@@ -35,14 +38,17 @@ export const BreadCrumb = ({
       {pathList &&
         pathList.map((item, idx, arr) => (
           <>
-            <Link to={LinkList[idx]}>
-              <Text
-                size="bodyS"
-                color={arr.length !== idx + 1 ? 'gray5' : 'gray10'}
-              >
+            {arr.length !== idx + 1 ? (
+              <Link to={LinkList[idx]}>
+                <Text size="bodyS" color={'gray5'}>
+                  {item}
+                </Text>
+              </Link>
+            ) : (
+              <Text size="bodyS" color={'gray10'} cursor="pointer">
                 {item}
               </Text>
-            </Link>
+            )}
             {arr.length !== idx + 1 && (
               <Arrow colorKey="gray5" direction="right" />
             )}
