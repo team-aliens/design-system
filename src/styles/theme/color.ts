152025ce6941e5c9bb3b8e5bbdb258fb8c87fdbc
kimjihwan8 <kimjihwan1592@naver.com>
2026-04-14 19:49:15 +0900

    feat: color styles 추가

--- src/styles/theme/color.ts
+++ src/styles/theme/color.ts
@@ -19,6 +19,45 @@ export const color = {
   error: '#ff4646',
   errorDarken1: '#c23535',
   errorDarken2: '#852424',
+  system: {
+    bg: "#F2F4F6",
+    button: "#B0B6C1",
+    container: "#FFFFFF",
+    hover: "#8D929A",
+    pressed: "#71757B"
+  },
+  gray: {
+    50: "#FFFFFF",
+    100: "#F9F9F9",
+    200: "#EEEEEE",
+    300: "#DDDDDD",
+    400: "#999999",
+    500: "#555555",
+    600: "#343434",
+    700: "#202020",
+    800: "#121212",
+    900: "#101010"
+  },
+  blue: {
+    50: "#E7F0FF",
+    100: "#B1D0FE",
+    200: "#65A2FE",
+    300: "#0F6EFE",
+    400: "#0C58CB",
+    500: "#052453",
+  },
+  red: {
+    50: "#FFE7E7",
+    100: "#FEB1B1",
+    200: "#FE6565",
+    300: "#FE0F0F",
+    400: "#CB0C0C",
+    500: "#530505",
+  },
+  etc: {
+    Button: "#FFFFFF",
+    disButton: "#B1D0FE"
+  }
 } as const;
 
 export const {
@@ -39,6 +78,11 @@ export const {
   error,
   errorLighten1,
   errorLighten2,
+  system,
+  gray,
+  blue,
+  red,
+  etc
 } = color;
 
 export type colorKeyOfType = keyof typeof color;
