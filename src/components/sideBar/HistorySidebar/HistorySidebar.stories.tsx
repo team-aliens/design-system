079b30b6b1de1b7120deecaf8afaab856b34ac05
kimjihwan8 <kimjihwan1592@naver.com>
2026-09-08 20:30:00 +0900

    chore: HistorySidebar 스토리에 상태 값 추가

--- src/components/sideBar/HistorySidebar/HistorySidebar.stories.tsx
+++ src/components/sideBar/HistorySidebar/HistorySidebar.stories.tsx
@@ -18,6 +18,7 @@ const data: StudyHistoryRequest[] = [
     student_name: '정일웅',
     student_gcn: '2312',
     teacher_name: '정은진',
+    status: 'FIRST_APPROVED',
   },
   {
     application_id: '2',
