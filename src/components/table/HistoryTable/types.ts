87c6c78f364a5d0bba8e371f3fb4ebb47b7c6a03
kimjihwan8 <kimjihwan1592@naver.com>
2026-09-09 22:10:00 +0900

    feat: HistoryTable에 onReachEnd prop 추가

--- src/components/table/HistoryTable/types.ts
+++ src/components/table/HistoryTable/types.ts
@@ -9,4 +9,6 @@ export interface HistoryTableItem {
 export interface HistoryTableProps {
   data: HistoryTableItem[];
   onRowClick: (id: string) => void;
+  /** 목록 끝까지 스크롤했을 때 호출된다. 다음 장을 불러오는 데 쓴다 */
+  onReachEnd?: () => void;
 }
